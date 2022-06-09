48820a2a765912a3b0bbaedf5a062e0371ca8e49
neriosoft <alabaneri@gmail.com>
2022-06-09 13:47:08 +0100

    first commit

---
+++ posts/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    poster = serializers.ReadOnlyField(source="poster.username")
+    poster_id = serializers.ReadOnlyField(source="poster.id")
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'url', 'poster','poster_id', 'created']
